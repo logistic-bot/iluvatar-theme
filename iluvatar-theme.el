5af57f960159bd1fabcdddeb453434865a3c5e9c
Khaïs COLIN <khais.colin@gmail.com>
2022-07-31 21:23:10 +0200

    Initial commit

---
+++ iluvatar-theme.el
@@ -0,0 +1,25 @@
+;;; iluvatar-theme.el ---- A theme for the gods -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2022 Khaïs COLIN
+;;
+;; Author: Khaïs COLIN <khais.colin@gmail.com>
+;; Maintainer: Khaïs COLIN <khais.colin@gmail.com>
+;; Created: July 31, 2022
+;; Modified: July 31, 2022
+;; Version: 0.0.1
+;; Keywords:
+;; Homepage: https://github.com/logistic-bot/iluvatar-theme
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;; Code:
+
+
+
+(provide 'iluvatar-theme)
+;;; iluvatar-theme.el ends here
